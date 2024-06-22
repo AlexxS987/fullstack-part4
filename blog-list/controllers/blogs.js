690dcac6fa703a3f52bd80afdff60f41c22ac20a
Aleksandr Skakun <m0405@student.jamk.fi>
2024-06-23 02:00:29 +0300

    Added DELETE and PUT routes with tests

--- blog-list/controllers/blogs.js
+++ blog-list/controllers/blogs.js
@@ -6,6 +6,11 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
+blogsRouter.get('/:id', async (request, response) => {
+  const blogs = await Blog.findById(request.params.id)
+  response.json(blogs)
+})
+
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
@@ -18,6 +23,28 @@ blogsRouter.post('/', async (request, response) => {
 
   const savedBlog = await blog.save()
   response.status(201).json(savedBlog)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(
+    request.params.id,
+    blog,
+    { new: true, runValidators: true, context: 'query' }
+  )
+  response.json(updatedBlog)
 })
 
 module.exports = blogsRouter
--- blog-list/tests/blog_api.test.js
+++ blog-list/tests/blog_api.test.js
@@ -9,124 +9,186 @@ const api = supertest(app)
 
 const Blog = require('../models/blog')
 
-beforeEach(async () => {
-  await Blog.deleteMany({})
-
-  for (let blog of helper.initialBlogs) {
-    let blogObject = new Blog(blog)
-    await blogObject.save()
-  }
-})
-
-describe('GET requests', () => {
-  test('all blogs are returned as json', async () => {
-    const response = await api
-      .get('/api/blogs')
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
-
-    assert.strictEqual(response.body.length, helper.initialBlogs.length)
+describe('when there is initially some notes saved', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(helper.initialBlogs)
   })
 
-  test('each blog has an id', async () => {
-    const response = await api.get('/api/blogs')
+  describe('GET requests', () => {
+    test('all blogs are returned as json', async () => {
+      const response = await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
 
-    const everyBlogHasId = response.body.every(blog => 'id' in blog)
+      assert.strictEqual(response.body.length, helper.initialBlogs.length)
+    })
 
-    assert(everyBlogHasId)
-  })
-})
+    test('each blog has an id', async () => {
+      const response = await api.get('/api/blogs')
 
-describe('good POST requests', () => {
-  test('a valid blog can be added', async () => {
-    const newBlog = {
-      _id: '5a422b3a1b54a676234d17f9',
-      title: 'Canonical string reduction',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-      likes: 12,
-      __v: 0
-    }
+      const everyBlogHasId = response.body.every(blog => 'id' in blog)
 
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
-
-    const blogsAtEnd = await helper.blogsInDb()
-    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
-
-    const contents = blogsAtEnd.map(b => b.title)
-    assert(contents.includes('Canonical string reduction'))
-  })
-})
-
-describe('malformed POST requests', () => {
-  test('blog without likes property defaults to 0', async () => {
-    const newBlog = {
-      _id: '5a422b3a1b54a676234d17f9',
-      title: 'Canonical string reduction',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-      __v: 0
-    }
-
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
-
-    const blogsAtEnd = await helper.blogsInDb()
-    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
-
-    const addedBlog = blogsAtEnd.find(b => b.title === 'Canonical string reduction')
-    assert.strictEqual(addedBlog.likes, 0)
+      assert(everyBlogHasId)
+    })
   })
 
-  test('blog with missing title return 400', async () => {
-    const newBlog = {
-      _id: '5a422b3a1b54a676234d17f9',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-      likes: 12,
-      __v: 0
-    }
+  describe('good POST requests', () => {
+    test('a valid blog can be added', async () => {
+      const newBlog = {
+        _id: '5a422b3a1b54a676234d17f9',
+        title: 'Canonical string reduction',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+        likes: 12,
+        __v: 0
+      }
 
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(400)
-      .expect((response) => {
-        assert(response.error, 'Blog validation failed: title: Path `title` is required.')
-      })
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
 
-    const blogsAtEnd = await helper.blogsInDb()
+      const blogsAtEnd = await helper.blogsInDb()
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
 
-    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+      const contents = blogsAtEnd.map(b => b.title)
+      assert(contents.includes('Canonical string reduction'))
+    })
   })
 
-  test('blog with missing url return 400', async () => {
-    const newBlog = {
-      _id: '5a422b3a1b54a676234d17f9',
-      title: 'Canonical string reduction',
-      author: 'Edsger W. Dijkstra',
-      likes: 12,
-      __v: 0
-    }
+  describe('malformed POST requests', () => {
+    test('blog without likes property defaults to 0', async () => {
+      const newBlog = {
+        _id: '5a422b3a1b54a676234d17f9',
+        title: 'Canonical string reduction',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+        __v: 0
+      }
 
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(400)
-      .expect((response) => {
-        assert(response.error, 'Blog validation failed: url: Path `url` is required.')
-      })
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
 
-    const blogsAtEnd = await helper.blogsInDb()
+      const blogsAtEnd = await helper.blogsInDb()
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
 
-    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+      const addedBlog = blogsAtEnd.find(b => b.title === 'Canonical string reduction')
+      assert.strictEqual(addedBlog.likes, 0)
+    })
+
+    test('blog with missing title return 400', async () => {
+      const newBlog = {
+        _id: '5a422b3a1b54a676234d17f9',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+        likes: 12,
+        __v: 0
+      }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+        .expect((response) => {
+          assert(response.error, 'Blog validation failed: title: Path `title` is required.')
+        })
+
+      const blogsAtEnd = await helper.blogsInDb()
+
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+    })
+
+    test('blog with missing url return 400', async () => {
+      const newBlog = {
+        _id: '5a422b3a1b54a676234d17f9',
+        title: 'Canonical string reduction',
+        author: 'Edsger W. Dijkstra',
+        likes: 12,
+        __v: 0
+      }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+        .expect((response) => {
+          assert(response.error, 'Blog validation failed: url: Path `url` is required.')
+        })
+
+      const blogsAtEnd = await helper.blogsInDb()
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+    })
+  })
+
+  describe('DELETE requests', () => {
+    test('succeeds with status code 204 if id is valid', async () => {
+      const blogsAtStart = await helper.blogsInDb()
+      const blogToDelete = blogsAtStart[0]
+
+      await api
+        .delete(`/api/blogs/${blogToDelete.id}`)
+        .expect(204)
+
+      const blogsAtEnd = await helper.blogsInDb()
+
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
+
+      const contents = blogsAtEnd.map(b => b.title)
+      assert(!contents.includes(blogToDelete.title))
+    })
+  })
+
+  describe('PUT requests', () => {
+    test('succeeds with status code 200 if id is valid', async () => {
+      const blogsAtStart = await helper.blogsInDb()
+      const blogToUpdate = blogsAtStart[0]
+      const updatedBlog = {
+        _id: '5a422a851b54a676234d17f7',
+        title: 'React patterns',
+        author: 'Michael Chan',
+        url: 'https://reactpatterns.com/',
+        likes: 10,
+        __v: 0
+      }
+
+      await api
+        .put(`/api/blogs/${blogToUpdate.id}`)
+        .send(updatedBlog)
+        .expect(200)
+
+      const blogsAtEnd = await helper.blogsInDb()
+
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+      assert.strictEqual(blogsAtEnd[0].likes, 10)
+    })
+
+    test('fails with status code 400 if id is not valid', async () => {
+      const updatedBlog = {
+        _id: '5a422a851b54a676234d17f7',
+        title: 'React patterns',
+        author: 'Michael Chan',
+        url: 'https://reactpatterns.com/',
+        likes: 10,
+        __v: 0
+      }
+
+      await api
+        .put('/api/blogs/1')
+        .send(updatedBlog)
+        .expect(400)
+        .expect((response) => {
+          assert(response.error, 'malformatted id')
+        })
+
+      const blogsAtEnd = await helper.blogsInDb()
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+    })
   })
 })
 
